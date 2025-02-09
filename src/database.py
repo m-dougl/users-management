006dde9ac5ebc5426cbcb37fa8d24e7e0cd500dc
m-dougl <douglas.maia.santos@itec.ufpa.br>
2025-02-09 17:02:32 -0300

    UPDATE: Added docstring

--- src/database.py
+++ src/database.py
@@ -1,20 +1,53 @@
+"""
+This module provides a PostgreSQL database connection using SQLAlchemy.
+"""
+
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
-import os 
+from logger import Logger
+
 load_dotenv()
+
 
 class PostgreSQL:
     def __init__(self):
-        self.hostname = os.getenv('DB_HOSTNAME')
-        self.user     = os.getenv('DB_USER')
-        self.password = os.getenv('DB_PASSWORD')
-        self.database = os.getenv('DB_DATABASE')
-        self.connection_str = f'postgresql+psycopg2://{self.user}:{self.password}@{self.hostname}/{self.database}'
-        
-    def get_engine(self):
-        try:
-            self.engine = create_engine(self.connection_str)
-            return self.engine
-        except Exception as e:
-            return e
+        """
+        Initialize database connection parameters and logger.
+        """
+        self.hostname = os.getenv("DB_HOSTNAME")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.database = os.getenv("DB_DATABASE")
+        self.connection_str = (
+            f"postgresql+psycopg2://{self.user}:{self.password}@"
+            f"{self.hostname}:5432/{self.database}"
+        )
 
+        self.logger_instance = Logger("database_logger", "database.log")
+        self.logger = self.logger_instance.get_logger()
+        self.engine = None
+
+    def get_engine(self):
+        """
+        Creates and returns a SQLAlchemy engine for the PostgreSQL database.
+
+        Returns:
+        engine: The SQLAlchemy engine for the PostgreSQL database.
+
+        Raises:
+        SQLAlchemyError: If an error occurs while creating the engine.
+        """
+
+        if not self.engine:
+            try:
+                self.engine = create_engine(self.connection_str)
+                self.logger.info("Connection successfully")
+                return self.engine
+
+            except SQLAlchemyError as e:
+                self.logger.error(f"Database connection failed: {str(e)}")
+                raise RuntimeError("Could not connect to the database")
+
+        return self.engine
